90810bc3593cf42a607400f26a55bb0a3ab3b426
Justin Grimes <justinlgrimes@gmail.com>
2019-10-16 20:30:19 -0400

    add switch to different c++ lib for OSX

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
-fn main() {
+use std::env;
+
+fn main() {    
+    let target = env::var("TARGET").unwrap();
+
     if let Some(datastax_dir) = option_env!("CASSANDRA_SYS_LIB_PATH") {
         for p in datastax_dir.split(";") {
             println!("cargo:rustc-link-search={}", p);
@@ -8,7 +12,11 @@ fn main() {
     println!("cargo:rustc-flags=-l dylib=cassandra");
     println!("cargo:rustc-flags=-l dylib=crypto");
     println!("cargo:rustc-flags=-l dylib=ssl");
-    println!("cargo:rustc-flags=-l dylib=stdc++");
+    if target.contains("apple") {
+        println!("cargo:rustc-link-lib=dylib=c++");
+    } else {
+        println!("cargo:rustc-flags=-l dylib=stdc++");
+    }
     println!("cargo:rustc-flags=-l dylib=uv");
     println!("cargo:rustc-link-search={}", "/usr/lib/x86_64-linux-gnu");
     println!("cargo:rustc-link-search={}", "/usr/local/lib/x86_64-linux-gnu");
